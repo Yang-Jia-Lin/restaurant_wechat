585e7df4bf735b47f31a750cc7d00c8e51849fa7
jialin <2239969828@qq.com>
2024-03-22 23:21:40 +0800

    修改门店管理布局

--- pages/Admin/adminStore/adminStore.js
+++ pages/Admin/adminStore/adminStore.js
@@ -4,24 +4,20 @@ let windowHeight = 0
 
 Page({
 	data: {
-		// 营业状态
 		business_status: '营业中',
 		takeout_status: '可配送',
-
-		// 菜品状态
+		shouldPrint: true,
 		menuArrIn: [],
 		menuArrOut: [],
-
-		// 打印机状态
-		shouldPrint: false
 	},
 
-	// 初始化各个状态
 	onLoad() {
 		this.getStoreInfo()
-		this.getFoodList()
 		this.getPrintingPreference()
+		this.getFoodList()
 	},
+
+	// 门店状态相关
 	getStoreInfo() {
 		wx.request({
 			url: baseUrl + 'stores/1',
@@ -47,6 +43,121 @@ Page({
 			}
 		});
 	},
+	onBusinessChange(e){
+		const newBusinessStatus = e.detail.value?'营业中':'休息中';
+		console.log(newBusinessStatus)
+		wx.request({
+			url: baseUrl + 'stores/1',
+			method: 'PUT',
+			data: {
+				business_status: newBusinessStatus
+			},
+			success: (res) => {
+				console.log(res.data)
+				if (res.data.success) {
+					this.setData({
+						business_status: res.data.updatedStore.business_status
+					})
+				} else {
+					wx.showToast({
+						title: '更新门店状态失败，请重试',
+						icon: 'none',
+					});
+				}
+			},
+			fail: (err) => {
+				console.log('更新门店状态失败', err);
+				wx.showToast({
+					title: '更新门店失败，请重试',
+					icon: 'none',
+				});
+			}
+		});
+	},
+	onTakeoutChange(e){
+		const newBusinessStatus = e.detail.value?'可配送':'暂不配送';
+		wx.request({
+			url: baseUrl + 'stores/1',
+			method: 'PUT',
+			data: {
+				takeout_status: newBusinessStatus
+			},
+			success: (res) => {
+				console.log(res.data)
+				if (res.data.success) {
+					this.setData({
+						takeout_status: res.data.updatedStore.takeout_status
+					})
+				} else {
+					wx.showToast({
+						title: '更新门店状态失败，请重试',
+						icon: 'none',
+					});
+				}
+			},
+			fail: (err) => {
+				console.log('更新门店状态失败', err);
+				wx.showToast({
+					title: '更新门店失败，请重试',
+					icon: 'none',
+				});
+			}
+		});
+	},
+
+	// 打印机相关
+	getPrintingPreference: function () {
+		wx.request({
+			url: baseUrl + 'setPrinter/get', // 替换为实际的后端接口地址
+			method: 'GET',
+			success: (res) => {
+				if (res.data.success) {
+					console.log('获取打印机状态成功', res.data)
+					this.setData({
+						shouldPrint: res.data.shouldPrint,
+					});
+				} else {
+					console.log('获取打印设置失败');
+				}
+			},
+			fail: () => {
+				console.log('请求失败');
+			}
+		});
+	},
+	onSwitchChange: function (e) {
+		const newShouldPrint = e.detail.value;
+		wx.request({
+			url: baseUrl + 'setPrinter/set',
+			method: 'POST',
+			data: {
+				shouldPrint: newShouldPrint,
+			},
+			success: (res) => {
+				if (res.data.success) {
+					console.log('修改打印机状态成功', res.data)
+					this.setData({
+						shouldPrint: newShouldPrint,
+					});
+				} else {
+					console.log('设置打印偏好失败');
+					wx.showToast({
+						title: '设置打印偏好失败，请重试',
+						icon: 'none',
+					});
+				}
+			},
+			fail: () => {
+				console.log('请求失败');
+				wx.showToast({
+					title: '设置打印偏好失败，请重试',
+					icon: 'none',
+				});
+			}
+		});
+	},
+
+	// 菜品状态相关
 	getFoodList() {
 		let menuArrIn = []; // 到店菜品数组
 		let menuArrOut = []; // 外卖菜品数组
@@ -80,70 +191,6 @@ Page({
 			}
 		});
 	},
-	getPrintingPreference: function () {
-		wx.request({
-			url: baseUrl + 'setPrinter/get', // 替换为实际的后端接口地址
-			method: 'GET',
-			success: (res) => {
-				if (res.data.success) {
-					console.log('获取打印机状态成功', res.data)
-					this.setData({
-						shouldPrint: res.data.shouldPrint,
-					});
-				} else {
-					console.log('获取打印设置失败');
-				}
-			},
-			fail: () => {
-				console.log('请求失败');
-			}
-		});
-	},
-
-
-
-	// 修改门店状态
-	onServiceTypeChange(e) {
-		console.log(e.currentTarget.dataset)
-		if (e.currentTarget.dataset.bstatus) {
-			this.changeServiceStatus("business_status", e.currentTarget.dataset.bstatus)
-		} else {
-			this.changeServiceStatus("takeout_status", e.currentTarget.dataset.tstatus)
-		}
-
-	},
-	changeServiceStatus(target, status) {
-		let data = {};
-		data[target] = status;
-		wx.request({
-			url: baseUrl + 'stores/1',
-			method: 'PUT',
-			data: data,
-			success: (res) => {
-				console.log(res.data.updatedStore)
-				if (res.data.success) {
-					this.setData({
-						business_status: res.data.updatedStore.business_status,
-						takeout_status: res.data.updatedStore.takeout_status
-					})
-				} else {
-					wx.showToast({
-						title: '更新门店状态失败，请重试',
-						icon: 'none',
-					});
-				}
-			},
-			fail: (err) => {
-				console.log('更新门店状态失败', err);
-				wx.showToast({
-					title: '更新门店失败，请重试',
-					icon: 'none',
-				});
-			}
-		});
-	},
-
-	// 修改菜品状态
 	goUpDish(e) {
 		const dish_id = e.currentTarget.dataset.id;
 		const serviceType = e.currentTarget.dataset.type;
@@ -163,31 +210,4 @@ Page({
 			}
 		})
 	},
-
-	// 修改打印机状态
-	onSwitchChange: function (e) {
-		const newShouldPrint = e.detail.value;
-		wx.request({
-			url: baseUrl + 'setPrinter/set',
-			method: 'POST',
-			data: {
-				shouldPrint: newShouldPrint,
-			},
-			success: (res) => {
-				if (res.data.success) {
-					console.log('修改打印机状态成功', res.data)
-					this.setData({
-						shouldPrint: newShouldPrint,
-					});
-				} else {
-					console.log('设置打印偏好失败');
-				}
-			},
-			fail: () => {
-				// 请求失败的处理
-				console.log('请求失败');
-			}
-		});
-	},
-
 })
